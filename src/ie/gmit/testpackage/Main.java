2aa3af1803547cd5f704d107f9b1b5f0c7b73dd8
Daniel Cregg <danielcregg@users.noreply.github.com>
2020-10-12 18:04:21 +0100

    Update Main.java

--- src/ie/gmit/testpackage/Main.java
+++ src/ie/gmit/testpackage/Main.java
@@ -1,7 +1,11 @@
 package ie.gmit.testpackage;
 
-public class TestClass {
+/**
+ * The Main class holds the main method.
+ */
+public class Main {
+	// Main method containing print statement.
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
-}
+		System.out.println("This is the main method, inside the Main calss.");
+	} // End main method
+} // End Main Class
